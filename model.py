1ae1ce2d61513a3fd8edb8ae3085944614fb094c
Victor I. Afolabi <javafolabi@gmail.com>
2018-06-07 19:57:54 +0100

    Command line arguments.

--- model.py
+++ model.py
@@ -24,14 +24,26 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-d', dest='data_dir', type=str, default='simulated_data',
+    # Data & checkpoint arguments.
+    parser.add_argument('-d', dest='data_dir', type=str, default='./simulations/',
                         help='Directory where simulated data is stored.')
+    parser.add_argument('-s', dest='save_path', type=str, default='./saved/model.ckpt',
+                        help='Checkpoint saved path.')
 
+    # Training arguments.
+    parser.add_argument('-e', dest='epochs', type=int, default=10000,
+                        help='Number of training epochs.')
+    parser.add_argument('-b', dest='batch_size', type=int, default=128,
+                        help='Mini-batch size.')
+    parser.add_argument('-lr', dest='learning_rate', type=float, default=1e-2,
+                        help='Optimizer\'s learning rate.')
+
+    # Parsed arguments.
     args = parser.parse_args()
 
-    print('{0}\nArguments\n{0}'.format('-' * 45))
+    print('{0}\n{1:^45}\n{0}'.format('-' * 45, 'Command Line Arguments'))
     for k, v in vars(args).items():
-        print('{:<20} = {}'.format(k, v))
+        print('{:<15} = {:>20}'.format(k, v))
     print('{}'.format('-' * 45))
 
     main(args=args)
