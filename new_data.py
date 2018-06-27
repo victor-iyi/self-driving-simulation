6f68b6890cdbd7fc6eeb31d620555e1f5a52e694
Victor I. Afolabi <javafolabi@gmail.com>
2018-06-27 20:05:54 +0100

    Creating new Data & Model class

---
+++ new_data.py
@@ -0,0 +1,38 @@
+"""Helper file for working with training dataset.
+
+   @author
+     Victor I. Afolabi
+     Artificial Intelligence & Software Engineer.
+     Email: javafolabi@gmail.com
+     GitHub: https://github.com/victor-iyiola
+
+   @project
+     File: data.py
+     Created on 08 June, 2018 @ 8:27 PM.
+
+   @license
+     MIT License
+     Copyright (c) 2018. Victor I. Afolabi. All rights reserved.
+"""
+
+
+class Data(object):
+
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+
+class Dataset(Data):
+    pass
---
+++ new_model.py
@@ -0,0 +1,21 @@
+"""Based on Nvidia's Paper on End-to-end learning for self driving car.
+
+   @author
+     Victor I. Afolabi
+     Artificial Intelligence & Software Engineer.
+     Email: javafolabi@gmail.com
+     GitHub: https://github.com/victor-iyiola/
+
+   @project
+     File: model.py
+     Created on 18 May, 2018 @ 5:26 PM.
+
+   @license
+     MIT License
+     Copyright (c) 2018. Victor I. Afolabi. All rights reserved.
+"""
+
+
+class Model(object):
+    def __init__(self):
+        pass
