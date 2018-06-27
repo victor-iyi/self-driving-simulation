12d77805ba20c315acb0d11837a2ba81ccd8231b
Victor I. Afolabi <javafolabi@gmail.com>
2018-06-27 21:18:00 +0100

    Data class

--- new_data.py
+++ new_data.py
@@ -7,13 +7,19 @@
      GitHub: https://github.com/victor-iyiola
 
    @project
-     File: data.py
+     File: new_data.py
      Created on 08 June, 2018 @ 8:27 PM.
 
    @license
      MIT License
      Copyright (c) 2018. Victor I. Afolabi. All rights reserved.
 """
+
+import os
+
+import cv2
+import numpy as np
+import pandas as pd
 
 
 class Data(object):
@@ -35,4 +41,57 @@ class Data(object):
 
 
 class Dataset(Data):
-    pass
+
+    # CSV File header names.
+    COL_NAMES = [
+        'center_path', 'left_path', 'right_path',  # Input
+        'steering_angle', 'throttle', 'brake', 'speed'  # Controls.
+    ]
+
+    def __init__(self, filename, **kwargs):
+        # Extract keyword arguments.
+        feature_col = kwargs.get('feature_col') or self.COL_NAMES[0]
+        label_col = kwargs.get('label_col') or self.COL_NAMES[-1]
+
+        if not os.path.isfile(filename):
+            raise FileNotFoundError('{} was not found!'.format(filename))
+
+        # Read csv into a pandas DataFrame object.
+        self._df = pd.read_csv(filename, names=self.COL_NAMES)
+
+        # Features & labels.
+        img_paths = self._df[feature_col].astype(str).values
+        label_paths = self._df[label_col].astype(np.float32).values
+
+        data = self._create_data(img_paths, label_paths)
+
+    def __repr__(self):
+        return "Dataset"
+
+    @classmethod
+    def fromArray(cls, arr):
+        pass
+
+    def _create_data(self, img_paths, label_paths):
+
+        yield img_paths, label_paths
+
+    def _process_img(self, img_path: str, size: tuple=(200, 200)):
+        # Load image from path.
+        image = cv2.imread(img_path)
+
+        # Resize image.
+        image = cv2.resize(image, size)
+
+        # Convert to RGB colors.
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        return image
+
+    @property
+    def df(self):
+        return self._df
+
+
+if __name__ == '__main__':
+    data = Dataset('simulations/driving_log.csv')
--- new_model.py
+++ new_model.py
@@ -15,7 +15,53 @@
      Copyright (c) 2018. Victor I. Afolabi. All rights reserved.
 """
 
+import tensorflow as tf
+from new_data import Dataset
+
 
 class Model(object):
-    def __init__(self):
+    def __init__(self, sess: tf.Session, data: Dataset):
+        self._sess = sess
+        self._data = data
+
+    def __call__(self, X, **kwargs):
+        return self.predict(X, **kwargs)
+
+    def train(self):
         pass
+
+    def predict(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def build_graph(self):
+        pass
+
+    def build_eval_graph(self):
+        pass
+
+    def _predict(self, X, **kwargs):
+        pass
+
+    class Result(object):
+        def __init__(self):
+            pass
+
+        def __len__(self):
+            pass
+
+        def __iter__(self):
+            pass
+
+        def __getitem__(self, idx):
+            pass
+
+    @property
+    def sess(self):
+        return self._sess
+
+    @property
+    def data(self):
+        return self._data
