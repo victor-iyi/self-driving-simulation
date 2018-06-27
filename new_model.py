138c0a436d8ebb45fa83b04e5d6e2b468e3284b6
Victor I. Afolabi <javafolabi@gmail.com>
2018-06-27 22:28:06 +0100

    Building model & dataset.

--- new_model.py
+++ new_model.py
@@ -20,9 +20,22 @@ from new_data import Dataset
 
 
 class Model(object):
-    def __init__(self, sess: tf.Session, data: Dataset):
+    def __init__(self, sess: tf.Session, data: Dataset, **kwargs):
         self._sess = sess
         self._data = data
+
+        self._verbose = kwargs.get('verbose', 1)
+        self._save_dir = kwargs.get('save_dir', 'saved/')
+        self._global_step = tf.train.get_or_create_global_step()
+
+        self.X_plhd = tf.placeholder(dtype=tf.float32, shape=(None,))
+        self.y_plhd = tf.placeholder(dtype=tf.float32, shape=(None,))
+
+        # self.build_graph()
+        # self.build_eval_graph()
+
+        # self._saver = tf.train.Saver()
+        self.restore()
 
     def __call__(self, X, **kwargs):
         return self.predict(X, **kwargs)
@@ -42,21 +55,25 @@ class Model(object):
     def build_eval_graph(self):
         pass
 
-    def _predict(self, X, **kwargs):
+    def restore(self):
+        # Restore checkpoint properly.
+        if tf.gfile.IsDirectory(self._save_dir):
+            try:
+                last_ckpt = tf.train.latest_checkpoint(self._save_dir)
+                self._saver.restore(sess=self._sess, save_path=last_ckpt)
+                print('Restored model checkpoint: {}'.format(last_ckpt))
+                return
+            except Exception:
+                print('WARN: Could not load checkpoint.')
+        else:
+            print('INFO: Creating checkpoint directory: {}'.format(ckpt_dir))
+            tf.gfile.MakeDirs(self._save_dir)
+
+        print('Initializing global variables instead.')
+        self._sess.run(tf.global_variables_initializer())
+
+    def _predict(self, **kwargs):
         pass
-
-    class Result(object):
-        def __init__(self):
-            pass
-
-        def __len__(self):
-            pass
-
-        def __iter__(self):
-            pass
-
-        def __getitem__(self, idx):
-            pass
 
     @property
     def sess(self):
