b8e4c797998590eb6c08b701f69791ca48e887b3
Victor I. Afolabi <javafolabi@gmail.com>
2018-06-29 20:05:19 +0100

    Model class.

--- new_model.py
+++ new_model.py
@@ -19,19 +19,34 @@ import tensorflow as tf
 from new_data import Dataset
 
 
+class Mode(object):
+    train = 'TRAIN'
+    predict = 'PREDICT'
+    evaluate = 'EVALUATE'
+
+
 class Model(object):
     def __init__(self, sess: tf.Session, data: Dataset, **kwargs):
-        self._sess = sess
-        self._data = data
-
+        # Extract keyword arguments.
         self._verbose = kwargs.get('verbose', 1)
+        self._dropout = kwargs.get('dropout', 0.5)
         self._save_dir = kwargs.get('save_dir', 'saved/')
-        self._global_step = tf.train.get_or_create_global_step()
+        self._img_dim = kwargs.get('img_dim', (200, 200, 3))
 
-        self.X_plhd = tf.placeholder(dtype=tf.float32, shape=(None,))
+        # Session & data.
+        self._sess, self._data = sess, data
+
+        # Placeholders.
+        self.X_plhd = tf.placeholder(dtype=tf.float32,
+                                     shape=(None, *self._img_dim))
         self.y_plhd = tf.placeholder(dtype=tf.float32, shape=(None,))
 
-        # self.build_graph()
+        # Model Hyperparameters.
+        self._mode = Mode.predict
+        self._num_classes = 1
+        self._global_step = tf.train.get_or_create_global_step()
+
+        self.build_graph()
         # self.build_eval_graph()
 
         # self._saver = tf.train.Saver()
@@ -39,6 +54,9 @@ class Model(object):
 
     def __call__(self, X, **kwargs):
         return self.predict(X, **kwargs)
+
+    def __repr__(self):
+        return '<Model mode={}>'.format(self._mode)
 
     def train(self):
         pass
@@ -50,7 +68,38 @@ class Model(object):
         pass
 
     def build_graph(self):
-        pass
+        with tf.name_scope('Model'):
+            with tf.name_scope('FeatureExtraction'):
+                # Layer 1.
+                net = tf.layers.conv2d(net, filters=16, kernel_size=5,
+                                       activation=tf.nn.relu, name="conv1")
+                net = tf.layers.max_pooling2d(net, pool_size=2, strides=2,
+                                              padding='same', name="pool1")
+                # Layer 2.
+                net = tf.layers.conv2d(net, filters=32, kernel_size=5,
+                                       activation=tf.nn.relu, name="conv2")
+                net = tf.layers.max_pooling2d(net, pool_size=2, strides=2,
+                                              padding='same', name="pool2")
+                # Layer 3.
+                net = tf.layers.conv2d(net, filters=64, kernel_size=3,
+                                       activation=tf.nn.relu, name="conv3")
+                net = tf.layers.max_pooling2d(net, pool_size=2, strides=2,
+                                              padding='same', name="pool3")
+
+            with tf.name_scope('FullyConnected'):
+                # Flatten & Dropout.
+                net = tf.layers.flatten(net)
+                net = tf.layers.dropout(net, rate=self._dropout,
+                                        training=self._mode == Mode.train)
+                # Fully connected layers.
+                net = tf.layers.dense(net, units=1024, activation='relu',
+                                      name='dense1')
+                net = tf.layers.dense(net, units=128, activation='relu')
+
+            with tf.name_scope('Prediction'):
+                # Prediction layer.
+                self.y_pred = tf.layers.dense(net, units=self._num_classes,
+                                              name='logits')
 
     def build_eval_graph(self):
         pass
@@ -85,4 +134,7 @@ class Model(object):
 
 
 if __name__ == '__main__':
-    pass
+    filename = 'simulations/driving_log.csv'
+    data = Dataset(filename)
+    with tf.Graph().as_default(), tf.Session() as sess:
+        model = Model(sess=sess, data=data)
